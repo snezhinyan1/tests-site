ff169d5fa1ec18993c2dce64624246f1bd918ae4
Mashaqq <brawllola28@gmail.com>
2025-05-11 15:27:56 +0800

    начальный коммит

---
+++ js/question.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+        text: "Выберите недопустимое название переменной в Питоне",
+        answers: [
+            "4a",
+            "classe",
+            "number",
+            "_"
+        ],
+        correct_answer: 0,
+        valor: 1
+    },
+    {
+        text: "Как правильно произносится название метода __init__?",
+        answers: [
+            "Магический метод init",
+            "init",
+            "дандер-инит",
+            "Конструктор"
+        ],
+        correct_answer: 2,
+        valor: 3
+    },
+    {
+        text: "Как легче всего записать весь латинский алфавит в Питоне?",
+        answers: [
+            "скопировать из интернета",
+            "ascii_lowercase из модуля string",
+            "написать вручную",
+            "str.alphabet"
+        ],
+        correct_answer: 1,
+        valor: 1
+    },
+]
